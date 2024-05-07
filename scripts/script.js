89ce3f16ce69fc9be2bc84345758b2927438113f
lgfas <luis.guilherme2811@gmail.com>
2024-05-06 21:33:00 -0300

    feat: added expertises and how sections

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.icon-menu');
-    const container = document.querySelector('.container');
-    const close = document.querySelector('.icon-close');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".icon-menu");
+  const container = document.querySelector(".container");
+  const close = document.querySelector(".icon-close");
 
-    hamburger.addEventListener('click', function() {
-        container.classList.toggle('menu-open');
-    });
-    close.addEventListener('click', function() {
-        container.classList.remove('menu-open')
-    })
+  hamburger.addEventListener("click", function () {
+    container.classList.toggle("menu-open");
+  });
+  close.addEventListener("click", function () {
+    container.classList.remove("menu-open");
+  });
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  //direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
